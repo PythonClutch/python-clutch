15c474fb25479f044e0199a26e5f0ec95c2bb0ec
Gordon Fierce <gordon.fierce@gmail.com>
2015-03-17 22:48:17 -0400

    Fix tests to correctly decode utf-8 bytestrings.

--- tests/test_api.py
+++ tests/test_api.py
@@ -3,7 +3,7 @@ import json
 
 def test_get_user_list(client, user):
     response = client.get("/api/v1/users")
-    user_list = json.loads(str(response.data))
+    user_list = json.loads(response.get_data().decode("utf-8"))
     user_data = user_list["data"][0]
     assert user_data["github_name"] == user.github_name
     assert user_data["github_url"] == user.github_url
@@ -11,6 +11,6 @@ def test_get_user_list(client, user):
 
 def test_get_user(client, user):
     response = client.get("/api/v1/users/" + str(user.id))
-    user_data = json.loads(str(response.data))
+    user_data = json.loads(response.get_data().decode("utf-8"))["data"]
     assert user_data["github_name"] == user.github_name
     assert user_data["github_url"] == user.github_url
