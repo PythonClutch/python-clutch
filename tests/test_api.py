3a416d2c75d40cab385c900a601e454fb791959b
Gordon Fierce <gordon.fierce@gmail.com>
2015-03-19 10:06:01 -0400

    Test api failure response.

--- tests/test_api.py
+++ tests/test_api.py
@@ -14,3 +14,10 @@ def test_get_user(client, user):
     user_data = json.loads(response.get_data().decode("utf-8"))["data"]
     assert user_data["github_name"] == user.github_name
     assert user_data["github_url"] == user.github_url
+
+
+def test_get_empty_projects(client, db):
+    response = client.get("api/v1/projects")
+    response_data = json.loads(response.get_data().decode("utf-8"))
+    assert response_data["status"] == "fail"
+    assert response_data["data"]["title"] == "There are no projects."
