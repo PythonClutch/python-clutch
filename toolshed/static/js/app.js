78c2fd7652234e6fbad7e595b7af21b7d961187b
jahammo2 <jahammo2@gmail.com>
2015-03-19 10:06:25 -0400

    small changes

--- toolshed/static/js/app.js
+++ toolshed/static/js/app.js
@@ -187,33 +187,6 @@ app.controller('NavCtrl', ['$location', function ($location) {
 
 }]);
 
-app.controller('ProjectCtrl', ['project', function (project) {
-
-	var self = this;
-
-	self.project = project;
-	
-}]);
-app.config(['$routeProvider', function($routeProvider) {    
-    var routeDefinition = {
-      templateUrl: 'static/project/project.html',
-      controller: 'ProjectCtrl',
-      controllerAs: 'vm',
-      resolve: {
-        project: ['$route', 'projectServices',
-          function($route, projectServices) {
-            var routeParams = $route.current.params;
-            return projectServices.getByProjectId(routeParams.projectid);
-          }]
-      }
-    };
-
-    $routeProvider.when('/home/projects/:projectid', routeDefinition);
-
-}]);
-
-
-
 app.factory('homeFactory', function () {
 
 	// var self = this;
@@ -301,6 +274,33 @@ app.factory('stringUtil', function() {
         }
     };
 });
+app.controller('ProjectCtrl', ['project', function (project) {
+
+	var self = this;
+
+	self.project = project;
+	
+}]);
+app.config(['$routeProvider', function($routeProvider) {    
+    var routeDefinition = {
+      templateUrl: 'static/project/project.html',
+      controller: 'ProjectCtrl',
+      controllerAs: 'vm',
+      resolve: {
+        project: ['$route', 'projectServices',
+          function($route, projectServices) {
+            var routeParams = $route.current.params;
+            return projectServices.getByProjectId(routeParams.projectid);
+          }]
+      }
+    };
+
+    $routeProvider.when('/home/projects/:projectid', routeDefinition);
+
+}]);
+
+
+
 app.controller('SubmitCtrl', function () {
 
 	var self = this;
