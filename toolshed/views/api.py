c882c7c31c166fde7d65af7556d99ecd49f37495
Zachary Huntington-Meath <zjhuntingtonmeath@gmail.com>
2015-03-25 12:57:35 -0400

    Added log api routes, and better project schemas.

--- toolshed/views/api.py
+++ toolshed/views/api.py
@@ -1,7 +1,7 @@
 import json
 from ..models import (User, UserSchema, Project, Like, ProjectSchema,
                       Comment, CommentSchema, Category, CategorySchema,
-                      Group, GroupSchema, LikeSchema)
+                      Group, GroupSchema, LikeSchema, LogSchema)
 from flask import Blueprint, jsonify, request, abort, url_for
 from ..extensions import db
 from .toolshed import require_login, current_user
@@ -18,7 +18,8 @@ api = Blueprint('api', __name__)
 
 all_users_schema = UserSchema(many=True)
 single_user_schema = UserSchema()
-all_projects_schema = ProjectSchema(many=True)
+all_projects_schema = ProjectSchema(many=True, exclude=("logs",))
+all_projects_with_logs = ProjectSchema(many=True)
 single_project_schema = ProjectSchema()
 single_comment_schema = CommentSchema()
 all_comments_schema = CommentSchema(many=True)
@@ -28,6 +29,8 @@ single_group_schema = GroupSchema()
 all_groups_schema = GroupSchema(many=True)
 single_like_schema = LikeSchema()
 all_likes_schema = LikeSchema(many=True)
+all_logs_schema = LogSchema(many=True)
+single_log_schema = LogSchema()
 
 # response functions
 
@@ -77,7 +80,7 @@ def get_pending_submissions(id):
         pending = Project.query.filter_by(submitted_by_id=user.id).filter_by(status=False).all()
         return success_response(all_projects_schema, pending)
     else:
-        return failure_response("No pending submissions.")
+        return failure_response("No pending submissions.", 404)
 
 
 @api.route("/users/<int:id>/submissions")
@@ -87,7 +90,7 @@ def get_submissions(id):
         submissions = Project.query.filter_by(submitted_by_id=user.id).filter_by(status=True).all()
         return success_response(all_projects_schema, submissions)
     else:
-        return failure_response("No submissions.")
+        return failure_response("No submissions.", 404)
 
 
 # project routes
@@ -115,7 +118,7 @@ def popular_projects():
     projects = Project.query.order_by(Project.score)
     if projects:
         return success_response(all_projects_schema, projects)
-    
+
 
 
 @api.route("/projects/<int:id>")
@@ -140,6 +143,27 @@ def make_project():
     db.session.add(project)
     db.session.commit()
     return success_response(single_project_schema, project)
+
+
+# Logs routes
+
+
+@api.route("/projects/logs")
+def project_logs():
+    projects = Project.query.all()
+    if projects:
+        return success_response(all_projects_with_logs, projects)
+    else:
+        return failure_response("There are no projects", 404)
+
+
+@api.route("/projects/<int:id>/logs")
+def project(id):
+    project = Project.query.get(id)
+    if project:
+        return success_response(single_project_schema, project)
+    else:
+        return failure_response("There was no such project.", 404)
 
 
 # Category routes
