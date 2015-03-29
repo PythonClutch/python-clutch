7074198ed61c08319199dd287516c0125d810bbd
Gordon Fierce <gordon.fierce@gmail.com>
2015-03-29 17:10:17 -0400

    Use parameters.

--- toolshed/updater.py
+++ toolshed/updater.py
@@ -22,10 +22,14 @@ try:
     github_lambda = os.environ['GITHUB_LAMBDA']
     pypi_lambda = os.environ['PYPI_LAMBDA']
     score_multiplier = os.environ['SCORE_SCALE']
+    use_log = os.environ['USE_LOG']
+    source_weight = float(os.environ['SOURCE_WEIGHT'])
 except:
     github_lambda = 0.005
     pypi_lambda = 0.005
     score_multiplier = 1
+    use_log = False
+    source_weight = 0.5
 
 
 def update_projects(projects):
@@ -99,13 +103,15 @@ def log_project(project):
 
 
 def update_projects_score(projects):
-    
+
     def raw_github_score(project):
         num_forks = project.forks_count
         num_watch = project.watchers_count
         time_delta = datetime.now() - project.last_commit
         days_since_last_commit = time_delta.days
         github_score = (num_forks + num_watch) * math.exp(-1 * days_since_last_commit * github_lambda)
+        if use_log:
+            github_score = math.log(github_score)
         return github_score
 
     def raw_pypi_score(project):
@@ -113,6 +119,8 @@ def update_projects_score(projects):
         time_delta = datetime.now() - project.current_version_release
         days_since_last_release = time_delta.days
         pypi_score = num_download * math.exp(-1 * days_since_last_release * pypi_lambda)
+        if use_log:
+            pypi_score = math.log(pypi_score)
         return pypi_score
 
     def get_best_pypi(projects):
@@ -133,10 +141,10 @@ def update_projects_score(projects):
             score = raw_pypi_score(project) / best_pypi
             if project.git_url:
                 git_score = raw_github_score(project) / best_github
-                score = score + git_score
-                project.score = (score / 2) * score_multiplier
             else:
-                project.score = (score / 2) * score_multiplier
+                git_score = 0
+            score = (score * (1-source_weight)) + (git_score * source_weight)
+            project.score = score * score_multiplier
         db.session.commit()
     set_scores(projects)
 
